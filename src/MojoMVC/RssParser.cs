f6b979dd8d8a1ae00877f94d2eab47cb96933fdb
mojofawad <mojofawad@gmail.com>
2025-08-17 01:22:15 -0400

    feat: add validation for RSS feed file path and update package references

--- src/MojoMVC/RssParser.cs
+++ src/MojoMVC/RssParser.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
+using Microsoft.Ajax.Utilities;
 using MojoMVC.Models;
 
 namespace MojoMVC
@@ -22,6 +24,15 @@ namespace MojoMVC
 
         public Feed GetRssFeed(string filePath)
         {
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentException("File path must not be null or empty.", nameof(filePath));
+            }
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"The file '{filePath}' does not exist.", filePath);
+            }
             using (var fileStream = new FileStream(filePath, FileMode.Open))
             {
                 var serializer = new XmlSerializer(typeof(RssFeed));
