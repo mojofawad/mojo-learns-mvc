4dfa79fc281c272d122443753cc30b60673d1cf3
mojofawad <mojofawad@gmail.com>
2025-09-07 16:20:07 -0400

    feat: implement RSS feed preview functionality with input validation
    * closes #7
    * closes #8
    * closes #9

--- src/MojoMVC/Controllers/PreviewController.cs
+++ src/MojoMVC/Controllers/PreviewController.cs
@@ -1,13 +1,28 @@
-﻿using System.Web.Mvc;
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using MojoMVC.Models.ViewModels.Forms;
 
 namespace MojoMVC.Controllers
 {
     public class PreviewController : Controller
     {
-        // GET
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Index(PreviewInput previewInput)
         {
-            return View();
+            return View(previewInput);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PreviewFeed(PreviewInput input)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var feed = await RssService.RetrieveRssFeed(input.FeedUrl);
+
+            return View(feed);
         }
     }
 }
