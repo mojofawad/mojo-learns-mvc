9f1811e28b278ee67c9cd3c85e636cc1b490e2ad
mojofawad <mojofawad@gmail.com>
2025-09-10 23:25:33 -0400

    feat: add feeds management with AddFeed action and UI integration

---
+++ src/MojoMVC/Controllers/FeedsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MojoMVC.Controllers
+{
+    public class FeedsController : Controller
+    {
+        [HttpPost]
+        public ActionResult AddFeed()
+        {
+            Console.WriteLine("AddFeed action called");
+            return new HttpStatusCodeResult(200);
+        }
+    }
+}
